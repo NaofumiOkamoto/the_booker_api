9788fb5eff29a68b663a1fd7758b9f138f34ba66
NaofumiOkamoto <n.okamooo@gmail.com>
2024-05-22 12:50:14 +0900

    update: 終了時間を秒まで取得

--- app/app.py
+++ app/app.py
@@ -1,3 +1,6 @@
+import datetime
+import time
+import re
 from flask import Flask,request,jsonify
 from database import init_db
 from flask_restful import Api
@@ -40,20 +43,34 @@ def index():
         else:
             liNum = 'li[3]'
         # tr[11] ~ tr[13] に終了日時がありそうなので以下のような取り方にした
-        th11 = driver.find_element(By.XPATH, f"//*[@id='l-sub']/div[2]/ul/{liNum}/section/div/div/table/tbody/tr[11]/th").text
-        td11 = driver.find_element(By.XPATH, f"//*[@id='l-sub']/div[2]/ul/{liNum}/section/div/div/table/tbody/tr[11]/td").text
-        th12 = driver.find_element(By.XPATH, f"//*[@id='l-sub']/div[2]/ul/{liNum}/section/div/div/table/tbody/tr[12]/th").text
-        td12 = driver.find_element(By.XPATH, f"//*[@id='l-sub']/div[2]/ul/{liNum}/section/div/div/table/tbody/tr[12]/td").text
-        th13 = driver.find_element(By.XPATH, f"//*[@id='l-sub']/div[2]/ul/{liNum}/section/div/div/table/tbody/tr[13]/th").text
-        td13 = driver.find_element(By.XPATH, f"//*[@id='l-sub']/div[2]/ul/{liNum}/section/div/div/table/tbody/tr[13]/td").text
-        for tr in [[th11, td11], [th12, td12], [th13, td13]]:
-            print(tr)
-            if tr[0] == '終了日時':
-                close_time = tr[1]
+        # th11 = driver.find_element(By.XPATH, f"//*[@id='l-sub']/div[2]/ul/{liNum}/section/div/div/table/tbody/tr[11]/th").text
+        # td11 = driver.find_element(By.XPATH, f"//*[@id='l-sub']/div[2]/ul/{liNum}/section/div/div/table/tbody/tr[11]/td").text
+        # th12 = driver.find_element(By.XPATH, f"//*[@id='l-sub']/div[2]/ul/{liNum}/section/div/div/table/tbody/tr[12]/th").text
+        # td12 = driver.find_element(By.XPATH, f"//*[@id='l-sub']/div[2]/ul/{liNum}/section/div/div/table/tbody/tr[12]/td").text
+        # th13 = driver.find_element(By.XPATH, f"//*[@id='l-sub']/div[2]/ul/{liNum}/section/div/div/table/tbody/tr[13]/th").text
+        # td13 = driver.find_element(By.XPATH, f"//*[@id='l-sub']/div[2]/ul/{liNum}/section/div/div/table/tbody/tr[13]/td").text
+        # for tr in [[th11, td11], [th12, td12], [th13, td13]]:
+        #     print(tr)
+        #     if tr[0] == '終了日時':
+        #         close_time = tr[1]
 
-        print('product_title: ', product_title)
-        print('current_price: ', current_price)
-        return jsonify({'success': True, 'title': product_title, 'current_price': current_price, 'close_time': close_time})
+        # 詳細ボタンを押して残り時間の詳細を取得する
+        detail_button = driver.find_element(By.XPATH, '//*[@id="l-sub"]/div[2]/ul/li[1]/div[1]/div[2]/div[1]/ul[1]/li[2]/a')
+        detail_button.click()
+        time.sleep(1.5)
+        driver.switch_to.window(driver.window_handles[-1])
+        remaining_time = driver.find_element(By.ID, 'time').text
+        pattern = r'\d+'
+        matches = re.findall(pattern, remaining_time)
+        days, hours, minutes, seconds = matches
+        now = datetime.datetime.now()
+        end_time = now + datetime.timedelta(days=int(days), hours=int(hours), minutes=int(minutes), seconds=int(seconds))
+        print('残り時間: ', remaining_time)
+        print('現在時刻: ', now)
+        print('終了時間: ', end_time)
+        print('商品名: ', product_title)
+        print('現在価格: ', current_price)
+        return jsonify({'success': True, 'title': product_title, 'current_price': current_price, 'close_time': end_time})
     except Exception as e:
         print('商品情報取得でエラー！！！！！！！！: ', e)
         return jsonify({'success': False, 'title': '商品が存在しません.....'})
