e87eff89824722b9e919c0a05059eea476119a57
NaofumiOkamoto <n.okamooo@gmail.com>
2024-06-28 16:52:37 +0900

    add log

--- app/test_scraping.py
+++ app/test_scraping.py
@@ -11,6 +11,7 @@ import time
 # Yahooにログインして入札する
 def hoge(auction_id, bid_first_amount):
   FILENAME = os.path.join(os.path.dirname(os.path.abspath(__file__)), f"images/{auction_id}.png")
+  error_place = ''
   try:
     options = webdriver.ChromeOptions()
     driver = webdriver.Remote(
@@ -28,38 +29,48 @@ def hoge(auction_id, bid_first_amount):
     time.sleep(1)
     print('-------driver get end-------')
     # id
+    error_place = 'id入力'
     login_bar = driver.find_element(By.ID, "login_handle")
     login_bar.send_keys('naofumi-okamoto-0803')
     next_button = driver.find_element(By.XPATH, "//*[@id='content']/div[1]/div/form/div[1]/div[1]/div[2]/div/button")
     next_button.click()
     time.sleep(1)
     # パスワード
+    error_place = 'パスワード入力'
     password_bar = driver.find_element(By.ID, "password")
     password_bar.send_keys('nnoo00')
     login_button = driver.find_element(By.XPATH, "//*[@id='content']/div[1]/div/form/div[2]/div/div[1]/div[2]/div[3]/button")
     login_button.click()
     time.sleep(1)
     # 商品ページへ
+    error_place = '商品ページ遷移'
     driver.get(f'https://page.auctions.yahoo.co.jp/jp/auction/{auction_id}')
     time.sleep(1)
     # ポップアップあったら消す
+    error_place = 'ポップアップ消す'
     prMdl = driver.find_element(By.XPATH, "//*[@id='js-prMdl-close']")
     prMdl.click()
     time.sleep(1)
     # 入札するボタン
+    error_place = '入札ボタン押す'
     bid_button = driver.find_element(By.XPATH, "//*[@id='l-sub']/div[2]/ul/li[1]/div[1]/div[2]/div[2]/div/div/a")
     bid_button.click()
     time.sleep(2)
     # 金額入力
+    error_place = '金額入力'
     bid_amount_input = driver.find_element(By.XPATH, "//*[@id='BidModals']/div[2]/div[2]/div[2]/form/div[1]/label/input")
     bid_amount_input.clear()
     bid_amount_input.send_keys(bid_first_amount)
     time.sleep(1)
     # 確認ボタン
+    error_place = '確認ボタン押す'
     confirm_button = driver.find_element(By.XPATH, "//*[@id='BidModals']/div[2]/div[2]/div[2]/form/div[3]/span/input")
     confirm_button.click()
     driver.save_screenshot(FILENAME)
     time.sleep(2)
+
+    # 入札後のxpath（入札を受け付けました。あなたが現在の最高額入札者です。）
+    # //*[@id="yaucBidAct"]/div[2]/div[1]/div[2]/div[2]/div[1]
     driver.quit()
 
     from app import app
@@ -82,6 +93,7 @@ def hoge(auction_id, bid_first_amount):
       now = datetime.now(pytz.timezone('Asia/Tokyo'))
       noww = now.strftime('%Y-%m-%d %H:%M:%S')
       book.bid_time = noww
+      book.error = f'{error_place} の箇所で "{e}"'
       if book:
         db.session.add(book)
         db.session.commit()
